599f539a3556e26d4a52f07b9d81896aa43db2bf
lrennels <lrennels@berkeley.edu>
2018-06-07 20:12:22 -0700

    Fix more paths

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -4,4 +4,4 @@ const SUITE = BenchmarkGroup()
 const benchmarkpath = normpath(joinpath(@__DIR__, "benchmark"))
 
 SUITE["region_models"] = BenchmarkGroup
-SUITE["region_models"] = @benchmarkable (include(joinpath(benchmarkpath, "RegionTutorialBenchmarks.jl")))
+SUITE["region_models"] = @benchmarkable (include("RegionTutorialBenchmarks.jl"))
