4ec82b863bb53de82cfcee4bd024cbb903580369
lrennels <lrennels@berkeley.edu>
2018-06-07 19:38:29 -0700

    Remove module:

--- benchmark/RegionTutorialBenchmarks.jl
+++ benchmark/RegionTutorialBenchmarks.jl
@@ -1,5 +1,3 @@
-module RegionTutorialBenchmarks
-
 const tutorialpath = normpath(joinpath(@__DIR__, "..", "examples", "tutorial"))
 const tworegionpath = joinpath(tutorialpath, "02-two-region-model")
 const oneregionpath = joinpath(tutorialpath, "01-one-region-model")
@@ -8,5 +6,3 @@ const oneregionpath = joinpath(tutorialpath, "01-one-region-model")
 
 # include(joinpath(oneregionpath, "one-region-model.jl"))
 # include(joinpath(tworegionpath, "main.jl"))
-
-end #module
